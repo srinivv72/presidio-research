cc8f3b51a9006493a4934037867b448e52fcd743
Venkat <venkat@baffle.io>
2025-08-16 19:53:31 +0530

    Eliminate the need for the FakeNameGenerator.com_3000.csv file

--- presidio_evaluator/data_generator/faker_extensions/datasets.py
+++ presidio_evaluator/data_generator/faker_extensions/datasets.py
@@ -1,10 +1,11 @@
 import random
 import re
+from faker import Faker
 
 import pandas as pd
 from pandas import DataFrame
 
-from presidio_evaluator.data_generator import raw_data_dir
+#from presidio_evaluator.data_generator import raw_data_dir
 
 
 def _camel_to_snake(name):
@@ -42,58 +43,89 @@ def _name_gendered(row):
     )
 
 
-def load_fake_person_df() -> DataFrame:
+def load_fake_person_df(records: int = 3000) -> DataFrame:
     """
-    :return: A DataFrame loaded with data from FakeNameGenerator.com, and cleaned to match faker conventions
+    Generate a minimal DataFrame with just name and gender data using Faker.
+    
+    :param records: Number of fake person records to generate (default: 3000)
+    :return: A DataFrame with fake person data containing only name and gender fields
     """
-    fake_person_data_path = raw_data_dir / "FakeNameGenerator.com_3000.csv"
-    fake_person_df = pd.read_csv(fake_person_data_path)
-    fake_person_df.columns = [_camel_to_snake(col) for col in fake_person_df.columns]
-    # Update some column names to fit Faker
-    fake_person_df.rename(
-        columns={"country": "country_code", "state": "state_abbr"}, inplace=True
-    )
-    fake_person_df.rename(
-        columns={
-            "country_full": "country",
-            "name_set": "nationality",
-            "street_address": "street_name",
-            "state_full": "state",
-            "given_name": "first_name",
-            "surname": "last_name",
-            "title": "prefix",
-            "email_address": "email",
-            "telephone_number": "phone_number",
-            "telephone_country_code": "country_calling_code",
-            "birthday": "date_of_birth",
-            "cc_number": "credit_card_number",
-            "cc_type": "credit_card_provider",
-            "cc_expires": "credit_card_expire",
-            "occupation": "job",
-            "domain": "domain_name",
-            "username": "user_name",
-            "zip_code": "zipcode",
-        },
-        inplace=True,
-    )
-    fake_person_df["person"] = fake_person_df.apply(_full_name, axis=1)
-    fake_person_df["name"] = fake_person_df["person"]
-    genderized = fake_person_df.apply(
-        lambda x: pd.Series(
-            _name_gendered(x),
-            index=[
-                "first_name_female",
-                "first_name_male",
-                "prefix_female",
-                "prefix_male",
-                "last_name_female",
-                "last_name_male",
-            ],
-        ),
-        axis=1,
-        result_type="expand",
-    )
-    # Remove credit card data, rely on Faker's as it is more realistic
-    del fake_person_df["credit_card_number"]
-    fake_person_df = pd.concat([fake_person_df, genderized], axis="columns")
-    return fake_person_df
+    fake = Faker()
+    
+    data = []
+    for _ in range(records):
+        gender = random.choice(['male', 'female'])
+        first_name = fake.first_name_male() if gender == 'male' else fake.first_name_female()
+        last_name = fake.last_name()
+            
+        data.append({
+            'first_name': first_name,
+            'last_name': last_name,
+            'gender': gender,
+            'name': f"{first_name} {last_name}",
+            'person': f"{first_name} {last_name}",
+            'first_name_male': first_name if gender == 'male' else '',
+            'first_name_female': first_name if gender == 'female' else '',
+            'last_name_male': last_name if gender == 'male' else '',
+            'last_name_female': last_name if gender == 'female' else '',
+            'prefix': 'Mr.' if gender == 'male' else random.choice(['Ms.', 'Mrs.']),
+            'prefix_male': 'Mr.' if gender == 'male' else '',
+            'prefix_female': random.choice(['Ms.', 'Mrs.']) if gender == 'female' else '',
+        })
+        
+    return pd.DataFrame(data)
+
+    """ def load_fake_person_df() -> DataFrame:
+        #:return: A DataFrame loaded with data from FakeNameGenerator.com, and cleaned to match faker conventions
+        fake_person_data_path = raw_data_dir / "FakeNameGenerator.com_3000.csv"
+        fake_person_df = pd.read_csv(fake_person_data_path)
+        fake_person_df.columns = [_camel_to_snake(col) for col in fake_person_df.columns]
+        # Update some column names to fit Faker
+        fake_person_df.rename(
+            columns={"country": "country_code", "state": "state_abbr"}, inplace=True
+        )
+        fake_person_df.rename(
+            columns={
+                "country_full": "country",
+                "name_set": "nationality",
+                "street_address": "street_name",
+                "state_full": "state",
+                "given_name": "first_name",
+                "surname": "last_name",
+                "title": "prefix",
+                "email_address": "email",
+                "telephone_number": "phone_number",
+                "telephone_country_code": "country_calling_code",
+                "birthday": "date_of_birth",
+                "cc_number": "credit_card_number",
+                "cc_type": "credit_card_provider",
+                "cc_expires": "credit_card_expire",
+                "occupation": "job",
+                "domain": "domain_name",
+                "username": "user_name",
+                "zip_code": "zipcode",
+            },
+            inplace=True,
+        )
+        fake_person_df["person"] = fake_person_df.apply(_full_name, axis=1)
+        fake_person_df["name"] = fake_person_df["person"]
+        genderized = fake_person_df.apply(
+            lambda x: pd.Series(
+                _name_gendered(x),
+                index=[
+                    "first_name_female",
+                    "first_name_male",
+                    "prefix_female",
+                    "prefix_male",
+                    "last_name_female",
+                    "last_name_male",
+                ],
+            ),
+            axis=1,
+            result_type="expand",
+        )
+        # Remove credit card data, rely on Faker's as it is more realistic
+        del fake_person_df["credit_card_number"]
+        fake_person_df = pd.concat([fake_person_df, genderized], axis="columns")
+        return fake_person_df
+    """
